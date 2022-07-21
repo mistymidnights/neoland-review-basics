00774f36c44cb920a67a64387e24ceadaa8b415b
mistymidnights <mistydeveloping@gmail.com>
2022-07-21 19:17:07 +0200

    Iteración #6: Función swap

--- review.js
+++ review.js
@@ -155,3 +155,18 @@ const rollDice =(caras)=>{
 }
 
 console.log (rollDice(12));
+
+
+//**Iteración #6: Función swap**
+//Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. 
+//La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante.
+
+let futbolistas= ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
+
+const swapWords = (array, index, index2) => {
+    let copia = array[index];
+        array[index] = array[index2];
+        array[index2] = copia;
+        return array;
+}
+console.log(swapWords(futbolistas, 1, 2));
